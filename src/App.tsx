eb2725aff0a069d02dd165fbdd10325ea0235f32
ParkByunggwon <startartart@naver.com>
2024-07-04 12:01:36 +0900

    feat: Shopping 컴포넌트 적용

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,15 @@
-function App() {
-  return <div>App</div>;
+import ShoppingContainer from './components/ShoppingHeader/ShoppingContainer';
+import ShoppingCommerceArea from './components/ShoppingCommerceArea/ShoppingCommerceArea';
+import PaginationFooter from './components/PaginationFooter/PaginationFooter';
+export default function App() {
+  return (
+    <div
+      className="w-custom absolute inset-y-1/4 right-1/2 translate-x-1/2
+    shadow-custom rounded-lg h-max"
+    >
+      <ShoppingContainer />
+      <ShoppingCommerceArea />
+      <PaginationFooter />
+    </div>
+  );
 }
-
-export default App;
