ac1dfce06dfc0c6408cd11ba9bf8f0b7bfa20792
ParkByunggwon <startartart@naver.com>
2024-07-11 13:33:16 +0900

    feat: 쇼핑 2-13페이지 레이아웃 구성

--- src/components/ShoppingCommerceArea/ShoppingCommerceDescription.tsx
+++ src/components/ShoppingCommerceArea/ShoppingCommerceDescription.tsx
@@ -1,4 +1,5 @@
 import { Fragment } from 'react/jsx-runtime';
+import { useShopStore } from '../../store/shoppingStore';
 
 const mailNameLink = [
   {
@@ -82,54 +83,77 @@ const quickLinkList = [
   },
 ];
 export default function ShoppingCommerceDescription() {
+  const { selected, currentPage } = useShopStore();
   return (
-    <div className="px-5 w-80 flex flex-col gap-3">
-      <div className="flex flex-col border rounded-lg bg-bar p-2.5 gap-1">
-        {mailNameLink.map((row) => {
-          return (
-            <div
-              key={row.id}
-              className="flex flex-row justify-between items-center"
-            >
-              {row.items.map((link, index) => {
-                return (
-                  <Fragment key={link.id}>
-                    <span className="hover:underline cursor-pointer">
-                      {link.name}
-                    </span>
-                    {index !== row.items.length - 1 && (
-                      <span className="inline-block w-1 h-1 rounded-full bg-dot"></span>
-                    )}
-                  </Fragment>
-                );
-              })}
-            </div>
-          );
-        })}
-      </div>
-      <div className="flex flex-row justify-between border rounded-lg bg-bar p-2.5">
-        {quickLinkList.map((link) => {
-          return (
-            <div
-              key={link.id}
-              className="flex flex-col justify-center items-center
-              gap-0.5 hover:underline cursor-pointer"
-            >
-              <div className="w-12 h-12 border rounded-full bg-white"></div>
-              <div>{link.name}</div>
-            </div>
-          );
-        })}
-      </div>
-      <div
-        className="flex flex-col gap-2 justify-center items-center
-      hover:underline cursor-pointer"
-      >
-        <div className="border rounded-lg h-40 w-full">
-          <img src="" alt="" />
+    <div className="px-5 w-80 flex flex-col gap-3 min-h-description">
+      {selected === 1 && currentPage >= 2 ? (
+        <div className="p-2.5 bg-brandShopColor border rounded flex flex-col gap-2.5">
+          <div className="flex gap-2 ">
+            <span className="text-lg font-bold">LF 스퀘어몰</span>
+            <span className="text-button">&gt;</span>
+          </div>
+          <div className="h-32 flex flex-row gap-2">
+            <img className="flex-1 border rounded" src="" alt="" />
+            <img className="flex-1 border rounded" src="" alt="" />
+          </div>
+          <div className="h-36 w-full">
+            <img className="border rounded w-full h-full" src="" alt="" />
+          </div>
+          <div>
+            <p>플라스틱 아일랜드 브랜드 기획전</p>
+            <p className="font-bold">68,780원</p>
+          </div>
         </div>
-        <p className="text-center">신동엽 특별가, 반바지도 자신감있게</p>
-      </div>
+      ) : (
+        <>
+          <div className="flex flex-col border rounded-lg bg-bar p-2.5 gap-1">
+            {mailNameLink.map((row) => {
+              return (
+                <div
+                  key={row.id}
+                  className="flex flex-row justify-between items-center"
+                >
+                  {row.items.map((link, index) => {
+                    return (
+                      <Fragment key={link.id}>
+                        <span className="hover:underline cursor-pointer">
+                          {link.name}
+                        </span>
+                        {index !== row.items.length - 1 && (
+                          <span className="inline-block w-1 h-1 rounded-full bg-dot"></span>
+                        )}
+                      </Fragment>
+                    );
+                  })}
+                </div>
+              );
+            })}
+          </div>
+          <div className="flex flex-row justify-between border rounded-lg bg-bar p-2.5">
+            {quickLinkList.map((link) => {
+              return (
+                <div
+                  key={link.id}
+                  className="flex flex-col justify-center items-center
+            gap-0.5 hover:underline cursor-pointer"
+                >
+                  <div className="w-12 h-12 border rounded-full bg-white"></div>
+                  <div>{link.name}</div>
+                </div>
+              );
+            })}
+          </div>
+          <div
+            className="flex flex-col gap-2 justify-center items-center
+    hover:underline cursor-pointer"
+          >
+            <div className="border rounded-lg h-40 w-full">
+              <img src="" alt="" />
+            </div>
+            <p className="text-center">신동엽 특별가, 반바지도 자신감있게</p>
+          </div>
+        </>
+      )}
     </div>
   );
 }
