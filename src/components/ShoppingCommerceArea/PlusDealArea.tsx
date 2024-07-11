98fef485e2ac9ede20458a19819fcf9fe5207387
ParkByunggwon <startartart@naver.com>
2024-07-11 13:32:14 +0900

    feat: 원쁠딜 쇼핑라이브 왼쪽 레이아웃 구성 및 구현

--- src/components/ShoppingCommerceArea/PlusDealArea.tsx
+++ src/components/ShoppingCommerceArea/PlusDealArea.tsx
@@ -1,3 +1,82 @@
+const examData = [
+  {
+    id: 1,
+    label: '1+1',
+    discount: '38%',
+    price: 24800,
+    title: '[캔마트] (시원해요) 헤링본 링클 반팔티 1개 + 1개',
+    img: '',
+  },
+  {
+    id: 2,
+    label: '적립',
+    discount: '22%',
+    price: 1660000,
+    title: '삼성 갤럭시북4 프로 NT960XGQ-A51A + SSD 더블업 사무용 휴대용',
+    img: '',
+  },
+  {
+    id: 3,
+    label: '1+@',
+    discount: '10%',
+    price: 141200,
+    title: '2024 하기스 네이처메이드 밤부 팬티형 3단계 3팩+3팩 외',
+    img: '',
+  },
+  {
+    id: 4,
+    label: '1+1',
+    discount: '38%',
+    price: 24800,
+    title: '[캔마트] (시원해요) 헤링본 링클 반팔티 1개 + 1개',
+    img: '',
+  },
+  {
+    id: 5,
+    label: '적립',
+    discount: '22%',
+    price: 1660000,
+    title: '삼성 갤럭시북4 프로 NT960XGQ-A51A + SSD 더블업 사무용 휴대용',
+    img: '',
+  },
+  {
+    id: 6,
+    label: '1+@',
+    discount: '10%',
+    price: 141200,
+    title: '2024 하기스 네이처메이드 밤부 팬티형 3단계 3팩+3팩 외',
+    img: '',
+  },
+];
+
 export default function PlusDealArea() {
-  return <>PlusDealArea</>;
+  return (
+    <div className="flex basis-2/3 flex-wrap gap-x-5 gap-y-6">
+      {examData.map((data) => {
+        return (
+          <div className="relative w-44" key={data.id}>
+            <div
+              className="absolute px-1.5 py-0.5 text-xs text-white bg-plusDeal
+              rounded-ss rounded-ee z-10"
+            >
+              {data.label}
+            </div>
+            <div
+              className="absolute py-0.5 px-2.5 text-xs text-white
+            rounded-ee translate-x-7 bg-discountLabel w-10 text-center"
+            >
+              {data.discount}
+            </div>
+            <div className="border rounded w-44 h-32">
+              <img src={data.img} alt="" />
+            </div>
+            <div className="line-clamp-2 text-sm mt-2.5">
+              <span className="text-liveNews font-bold">{data.price}원</span>
+              <span> {data.title}</span>
+            </div>
+          </div>
+        );
+      })}
+    </div>
+  );
 }
--- src/components/ShoppingCommerceArea/ShoppingLiveArea.tsx
+++ src/components/ShoppingCommerceArea/ShoppingLiveArea.tsx
@@ -1,3 +1,124 @@
+import { useState } from 'react';
+
+const liveData = [
+  {
+    id: 1,
+    label: '어제인기',
+    discount: '16%',
+    view: 683232,
+    price: 199000,
+    title: '루메나 무소음 BLDC 무선 선풍',
+    subTitle: '브랜드데이 기념, 루메나 쇼핑라이브️',
+    img: '',
+    video: '',
+  },
+  {
+    id: 2,
+    label: '오늘인기',
+    discount: '42%',
+    view: 923213,
+    price: 66000,
+    title: '하이뮨 프로틴 밸런스 액티브 25',
+    subTitle: '하이뮨 썸머위크',
+    img: '',
+    video: '',
+  },
+  {
+    id: 3,
+    label: '인기라이브',
+    discount: '29%',
+    view: 554442,
+    price: 69850,
+    title: '루메나 무소음 BLDC 무선 선풍',
+    subTitle: '[내일뭐입지]룩캐스트 24신상 최대 혜택 라이브',
+    img: '',
+    video: '',
+  },
+  {
+    id: 4,
+    label: '오늘인기',
+    discount: '42%',
+    view: 923213,
+    price: 66000,
+    title: '하이뮨 프로틴 밸런스 액티브 25',
+    subTitle: '하이뮨 썸머위크',
+    img: '',
+    video: '',
+  },
+  {
+    id: 5,
+    label: '인기라이브',
+    discount: '29%',
+    view: 554442,
+    price: 69850,
+    title: '루메나 무소음 BLDC 무선 선풍',
+    subTitle: '[내일뭐입지]룩캐스트 24신상 최대 혜택 라이브',
+    img: '',
+    video: '',
+  },
+  {
+    id: 6,
+    label: '어제인기',
+    discount: '16%',
+    view: 683232,
+    price: 199000,
+    title: '루메나 무소음 BLDC 무선 선풍',
+    subTitle: '브랜드데이 기념, 루메나 쇼핑라이브️',
+    img: '',
+    video: '',
+  },
+];
 export default function ShoppingLiveArea() {
-  return <div>ShoppingLiveArea</div>;
+  const [current, setCurrent] = useState(1);
+
+  const currentData = liveData.find((item) => item.id === current);
+
+  return (
+    <div className="flex basis-2/3 gap-4">
+      <div className="flex flex-col gap-3">
+        <div className="relative w-60 h-80 border rounded">
+          <div className="absolute left-2 top-2 border rounded-full px-2 py-1 bg-label text-white">
+            <p>523,220명 시청</p>
+          </div>
+          <img src="" alt="" />
+          <div
+            className="absolute right-4 bottom-4 border rounded-full
+          w-8 h-8 bg-label"
+          ></div>
+        </div>
+        <div className="flex gap-3 w-60">
+          <div className="w-12 h-12 border rounded">
+            <img src="" alt="" />
+          </div>
+          <div>
+            <p className="line-clamp-1">{currentData?.title}</p>
+            <span className="text-liveNews font-bold">
+              {currentData?.discount}
+            </span>
+            <span className="font-bold">{currentData?.price}원</span>
+          </div>
+        </div>
+      </div>
+      <div className="flex flex-wrap gap-4">
+        {liveData.map((item) => {
+          return (
+            <div key={item.id} className="relative w-24">
+              <div
+                className="absolute left-2 top-2 border rounded-full text-sm
+              bg-label text-white font-bold"
+              >
+                <p className="px-2">{item.label}</p>
+              </div>
+              <div className="h-32 border rounded">
+                <img src="" alt="" />
+              </div>
+              <div>
+                <span className="line-clamp-2">{item.subTitle}</span>
+              </div>
+            </div>
+          );
+        })}
+      </div>
+    </div>
+  );
 }
