7d2ba5f53713f505b13e13c1eb685c52fcfcb92f
ParkByunggwon <startartart@naver.com>
2024-07-11 13:33:44 +0900

    feat: 컴포넌트 적용 및 앱위치 수정

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import PaginationFooter from './components/PaginationFooter/PaginationFooter';
 export default function App() {
   return (
     <div
-      className="w-custom absolute inset-y-1/4 right-1/2 translate-x-1/2
+      className="w-custom absolute top-20 right-1/2 translate-x-1/2
     shadow-custom rounded-lg h-max"
     >
       <ShoppingContainer />
--- src/components/ShoppingCommerceArea/ShoppingCommerceArea.tsx
+++ src/components/ShoppingCommerceArea/ShoppingCommerceArea.tsx
@@ -7,12 +7,14 @@ import SideArea from './SideArea';
 import ShoppingLiveArea from './ShoppingLiveArea';
 
 export default function ShoppingCommerceArea() {
-  const { headerMenu, selected } = useShopStore();
+  const { selected } = useShopStore();
   return (
     <div className="flex flex-col">
-      {/* 쇼핑, 맨즈 */}
       <div className="min-h-main">
+        {/* 원쁠딜, 쇼핑라이브 */}
         {selected >= 3 && <TodayPickArea />}
+
+        {/* 쇼핑, 맨즈 */}
         {selected <= 2 && (
           <>
             <div className="flex gap-5">
@@ -22,19 +24,19 @@ export default function ShoppingCommerceArea() {
             <TodayPickArea />
           </>
         )}
+
+        {selected >= 3 && (
+          <div className="flex px-5 h-max">
+            {/* 원쁠딜 */}
+            {selected === 3 && <PlusDealArea />}
+            {/* 쇼핑라이브 */}
+            {selected === 4 && <ShoppingLiveArea />}
+            <div className="border-s"></div>
+            {/* 원쁠딜, 쇼핑라이브 */}
+            <SideArea />
+          </div>
+        )}
       </div>
     </div>
   );
 }
-
-// {/* 원쁠딜, 쇼핑라이브 */}
-//
-
-// {/* 원쁠딜 */}
-// <PlusDealArea />
-
-// {/* 쇼핑라이브 */}
-// <ShoppingLiveArea />
-
-// {/* 원쁠딜, 쇼핑라이브 */}
-// <SideArea />
