0f317ad2072349078d5e7286d962e5c4fd218f84
ParkByunggwon <startartart@naver.com>
2024-07-11 13:31:37 +0900

    feat: PaginationBar 구현

--- src/components/ShoppingCommerceArea/TodayPickArea/PaginationMainBar.tsx
+++ src/components/ShoppingCommerceArea/TodayPickArea/PaginationMainBar.tsx
@@ -1,3 +1,47 @@
-export default function PaginationMainBar() {
-  return <div>PaginationMainBar</div>;
+import { useShopStore } from '../../../store/shoppingStore';
+import { Fragment } from 'react/jsx-runtime';
+
+type NavType = {
+  id: number;
+  move: number;
+  name: string;
+};
+type PaginationMainBarProps = {
+  list: NavType[];
+};
+
+export default function PaginationMainBar({ list }: PaginationMainBarProps) {
+  const { currentPage, pagination } = useShopStore();
+
+  const boldItemId = list.reduce((acc, nav) => {
+    if (currentPage >= nav.move) return nav;
+    return acc;
+  }, list[0]).id;
+
+  const handleNavigate = (move: number) => {
+    pagination(move);
+  };
+  return (
+    <>
+      {list.map((item, index) => {
+        return (
+          <Fragment key={item.id}>
+            <span
+              className={
+                boldItemId === item.id
+                  ? 'font-extrabold	hover:underline cursor-pointer text-navNameActive'
+                  : 'font-semibold hover:underline cursor-pointer text-navName'
+              }
+              onClick={() => handleNavigate(item.move)}
+            >
+              {item.name}
+            </span>
+            {index !== list.length - 1 && (
+              <span className="inline-block w-1 h-1 rounded-full bg-dot text-navName"></span>
+            )}
+          </Fragment>
+        );
+      })}
+    </>
+  );
 }
--- src/components/ShoppingCommerceArea/TodayPickArea/TodayPickArea.tsx
+++ src/components/ShoppingCommerceArea/TodayPickArea/TodayPickArea.tsx
@@ -1,25 +1,103 @@
+import { useShopStore } from '../../../store/shoppingStore';
+import PaginationMainBar from './PaginationMainBar';
+
+const onePluseDealNav = [
+  {
+    id: 1,
+    move: 1,
+    name: 'HOT',
+  },
+  {
+    id: 2,
+    move: 6,
+    name: '식품',
+  },
+  {
+    id: 3,
+    move: 8,
+    name: '생활·육아',
+  },
+  {
+    id: 4,
+    move: 9,
+    name: '패션소호',
+  },
+  {
+    id: 5,
+    move: 10,
+    name: '디지털',
+  },
+  {
+    id: 6,
+    move: 11,
+    name: '패션뷰티',
+  },
+  {
+    id: 7,
+    move: 12,
+    name: '오픈예정',
+  },
+];
+
+const shoppingLiveNav = [
+  {
+    id: 1,
+    move: 1,
+    name: '네쇼라Pick',
+  },
+  {
+    id: 2,
+    move: 2,
+    name: '패션',
+  },
+  {
+    id: 3,
+    move: 3,
+    name: '뷰티',
+  },
+  {
+    id: 4,
+    move: 4,
+    name: '푸드',
+  },
+  {
+    id: 5,
+    move: 5,
+    name: '라이프/테크',
+  },
+  {
+    id: 6,
+    move: 6,
+    name: '키즈',
+  },
+  {
+    id: 7,
+    move: 7,
+    name: '취미/여행/문화',
+  },
+];
 export default function TodayPickArea() {
+  const { selected } = useShopStore();
   return (
     <div
       className="mx-5 my-3 px-5 py-3 bg-todayPick
     flex flex-row items-center gap-2 text-lg font-medium"
     >
-      {/* 
-      <쇼핑, 맨즈>
-      오늘의 혜택
-      dot
-      logo
-      (date) description <hotKeyword>
-
-      <원쁠딜, 쇼핑라이브>
-       */}
-      <span className="font-bold">오늘의 혜택</span>
-      <span className="inline-block w-1 h-1 rounded-full bg-dot"></span>
-      <span>Logo</span>
-      <span className="hover:underline">
-        7/4~7/10 7월 혜택이 무려 <span className="text-unique">50% 세일</span>{' '}
-        이랍니다.
-      </span>
+      {selected <= 2 ? (
+        <>
+          <span className="font-bold">오늘의 혜택</span>
+          <span className="inline-block w-1 h-1 rounded-full bg-dot"></span>
+          <span>Logo</span>
+          <span className="hover:underline">
+            7/4~7/10 7월 혜택이 무려{' '}
+            <span className="text-unique">50% 세일</span> 이랍니다.
+          </span>
+        </>
+      ) : (
+        <PaginationMainBar
+          list={selected === 3 ? onePluseDealNav : shoppingLiveNav}
+        />
+      )}
     </div>
   );
 }
